af65317067b6f527c113d77290910d717601f791
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-05 15:34:44 +0200

    first update

---
+++ section-2-1-STARTER/app.js
@@ -0,0 +1,17 @@
+Vue.createApp({
+    data() {
+        return {
+            firstName: "john",
+            lastName: "doe"
+        }
+    }
+}).mount('#app')
+
+Vue.createApp({
+    data() {
+        return {
+            firstName: "james",
+            lastName: "doe"
+        }
+    }
+}).mount('#app01')
